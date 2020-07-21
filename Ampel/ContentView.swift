c9941f81ceb4a69c82fa530fb4fba5f5f4e0c4a7
Aleksandr Kurdiukov <runoleonard@gmail.com>
2020-07-21 20:07:41 +0200

    Initial Commit

---
+++ Ampel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ampel
+//
+//  Created by Aleksandr Kurdiukov on 21.07.20.
+//  Copyright © 2020 Zizlak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
